7aa962b4f826e17274c49752f3fd2e157a6bdc3c
Андрей Варанкин <vrnandr@gmail.com>
2021-02-18 21:48:06 +0400

    add bottom navigation

--- app/src/main/java/com/example/vrnandr/kpiwatcher/MainActivity.kt
+++ app/src/main/java/com/example/vrnandr/kpiwatcher/MainActivity.kt
@@ -7,6 +7,10 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
@@ -15,6 +19,7 @@ import com.example.vrnandr.kpiwatcher.ui.main.DetailFragment
 import com.example.vrnandr.kpiwatcher.ui.main.LoginFragment
 import com.example.vrnandr.kpiwatcher.ui.main.MainFragment
 import com.example.vrnandr.kpiwatcher.worker.UpdateWorker
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
 import java.util.jar.Manifest
@@ -32,21 +37,32 @@ class MainActivity : AppCompatActivity(), MainFragment.Callbacks, LoginFragment.
 
         val login = repo.getLogin()
         if (savedInstanceState == null) {
-            if (login!=null){
+            if (login!=null) {
                 val updateWorker = PeriodicWorkRequestBuilder<UpdateWorker>(repo.getTimer(), TimeUnit.MINUTES).build()
                 WorkManager.getInstance(this).apply {
                     cancelAllWork()
-                    enqueueUniquePeriodicWork(WORKER_TAG, ExistingPeriodicWorkPolicy.KEEP,updateWorker)
+                    enqueueUniquePeriodicWork(WORKER_TAG, ExistingPeriodicWorkPolicy.KEEP, updateWorker)
                 }
-                supportFragmentManager.beginTransaction()
+            }
+            /*    supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
             } else {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.container, LoginFragment.newInstance())
                     .commitNow()
-            }
+            }*/
         }
+
+        val navView: BottomNavigationView =findViewById(R.id.nav_view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.detailFragment, R.id.mainFragment, R.id.loginFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
         checkPermission()
     }
 
