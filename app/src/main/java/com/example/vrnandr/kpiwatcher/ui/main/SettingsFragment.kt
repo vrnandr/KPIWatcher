86ccb90b8b982aba62762d65acce7070d8449a44
andrey.varankin <andrey.varankin@realtimetech.ru>
2021-02-20 15:57:29 +0400

    exchange commit

--- app/src/main/java/com/example/vrnandr/kpiwatcher/ui/main/SettingsFragment.kt
+++ app/src/main/java/com/example/vrnandr/kpiwatcher/ui/main/SettingsFragment.kt
@@ -1,30 +1,17 @@
 package com.example.vrnandr.kpiwatcher.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.example.vrnandr.kpiwatcher.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    companion object {
-        fun newInstance() = SettingsFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        preferenceManager.sharedPreferencesName = "settings"
     }
 
-    private lateinit var viewModel: SettingsViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.settings_fragment, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
--- app/src/main/java/com/example/vrnandr/kpiwatcher/ui/main/SettingsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.vrnandr.kpiwatcher.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class SettingsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
